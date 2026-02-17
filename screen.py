ff5e20292bcc37fd482ca6aa4c366b69869ec416
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-17 13:22:25 +0000

    Add files via upload

--- screen.py
+++ screen.py
@@ -1,15 +1,13 @@
-var1="""Workstation.GetScreenResolution MonitorNumber: 1 MonitorWidth=> MonitorWidth MonitorHeight=> MonitorHeight MonitorBitCount=> MonitorBitCount MonitorFrequency=> MonitorFrequency
-Text.ToNumber Text: MonitorWidth Number=> varxxx
-Text.ToNumber Text: MonitorHeight Number=> varyyy
-SET varx TO %varxxx% / $8
-SET vary TO %varyyy% / $9
+var1="""Workstation.GetScreenResolution MonitorNumber: 1 MonitorWidth => MonitorWidth MonitorHeight => MonitorHeight
+SET varx TO %MonitorWidth% / 8
+SET vary TO %MonitorHeight% / 6
 Display.ShowMessageDialog.ShowMessage Title: $'''x,y''' Message: $'''%varx% , %vary%''' Icon: Display.Icon.None Buttons: Display.Buttons.OK DefaultButton: Display.DefaultButton.Button1 IsTopMost: False ButtonPressed=> ButtonPressed
 """
 print("\033c\033[40;37m\n give me x div by screen' ? ")
 a=input().strip()
 print("\033[40;37m\n give me y div by screen  ")
 b=input().strip()
-var1=var1.replace("$8",a)
-var1=var1.replace("$9",b)
+var1=var1.replace("$0",a)
+var1=var1.replace("$1",b)
 print("\n"+"-"*80 +"\n")
 print(var1)
