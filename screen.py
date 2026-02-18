7d70d4a7e0e3d5b7ef0be832afb57aa8f45da715
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-18 02:39:19 +0000

    Add files via upload

--- screen.py
+++ screen.py
@@ -1,13 +1,18 @@
-var1="""Workstation.GetScreenResolution MonitorNumber: 1 MonitorWidth => MonitorWidth MonitorHeight => MonitorHeight
-SET varx TO MonitorWidth / $0
-SET vary TO MonitorHeight / $1
-Display.ShowMessageDialog.ShowMessage Title: $'''x,y''' Message: $'''%varx% , %vary%''' Icon: Display.Icon.None Buttons: Display.Buttons.OK DefaultButton: Display.DefaultButton.Button1 IsTopMost: False ButtonPressed=> ButtonPressed
+var1="""SET progman TO $'''pbrush.exe'''
+System.RunApplication.RunApplication ApplicationPath: progman WindowStyle: System.ProcessWindowStyle.Maximized ProcessId=> AppProcessId
+Workstation.GetScreenResolution MonitorNumber: 1 MonitorWidth=> MonitorWidth MonitorHeight=> MonitorHeight
+SET varx TO MonitorWidth / 2
+SET vary TO MonitorHeight / 2 + 62
+WAIT 3
+LOOP LoopIndex FROM -20 TO 20 STEP 1
+    MouseAndKeyboard.MoveMouse X: varx + LoopIndex Y: vary + LoopIndex RelativeTo: MouseAndKeyboard.PositionRelativeTo.ActiveWindow MovementStyle: MouseAndKeyboard.MovementStyle.Instant
+    MouseAndKeyboard.SendMouseClick.Click ClickType: MouseAndKeyboard.MouseClickType.LeftClick MillisecondsDelay: 0
+END
+LOOP LoopIndex FROM -20 TO 20 STEP 1
+    MouseAndKeyboard.MoveMouse X: varx + LoopIndex Y: vary - LoopIndex RelativeTo: MouseAndKeyboard.PositionRelativeTo.ActiveWindow MovementStyle: MouseAndKeyboard.MovementStyle.Instant
+    MouseAndKeyboard.SendMouseClick.Click ClickType: MouseAndKeyboard.MouseClickType.LeftClick MillisecondsDelay: 0
+END
 """
-print("\033c\033[40;37m\n give me x div by screen' ? ")
-a=input().strip()
-print("\033[40;37m\n give me y div by screen  ")
-b=input().strip()
-var1=var1.replace("$0",a)
-var1=var1.replace("$1",b)
+print("\033c\033[40;37m\n ")
 print("\n"+"-"*80 +"\n")
 print(var1)
