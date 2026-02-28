cb184410d7ce9f484c26b31e023574a6781564a6
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-28 15:41:06 +0000

    Add files via upload

---
+++ num.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from scipy import stats
+
+plt.style.use('dark_background')
+
+# Dados
+x = list(range(20))
+y = [int(float(v) * 2.25) for v in x]
+
+# Regressão linear
+slope, intercept, r, p, std_err = stats.linregress(x, y)
+
+def myfunc(x):
+    return slope * x + intercept
+
+mymodel = list(map(myfunc, x))
+print(x)
+print(y)
+# Gráfico
+plt.scatter(x, y)
+plt.plot(x, mymodel)
+plt.show()
