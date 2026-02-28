831cd7e07a03d0a69412616a602aa7ff6a7a88e1
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-28 20:27:58 +0000

    Add files via upload

--- num.py
+++ num.py
@@ -5,21 +5,15 @@ plt.style.use('dark_background')
 
 # Dados
 x = list(range(20))
-y = [int(float(v) * 2.25) for v in x]
+y = [float(v) * 2.25 for v in x]
 
 # Regressão linear
 slope, intercept, r, p, std_err = stats.linregress(x, y)
 
 def myfunc(x):
     return slope * x + intercept
-print(slope)
-print(intercept)
-print(r)
-print(p)
 mymodel = list(map(myfunc, x))
-print(x)
-print(y)
-print(mymodel)
+
 # Gráfico
 plt.scatter(x, y)
 plt.plot(x, mymodel)
